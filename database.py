c3122a236470661abb6fee98f88abb587aeee9af
Irakli <irakli.ivanidze86@gmail.com>
2026-09-18 13:21:01 +0400

    Edited syntax error

--- database.py
+++ database.py
@@ -166,10 +166,10 @@ def get_bills(limit,offset, search):
     cursor.execute("""
     SELECT *
     FROM bills
-    WHERE bill_name ILIKE '%' || %s || '%'
+    WHERE bill_name ILIKE %s
     LIMIT %s
     OFFSET %s
-""", (search, limit, offset))
+""", (f"%{search}%", limit, offset))
 
     # ვინახავთ ყველა ჩანაწერს
     bills = cursor.fetchall()
